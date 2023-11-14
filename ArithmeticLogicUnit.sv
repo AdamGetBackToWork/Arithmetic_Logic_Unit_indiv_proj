135c79b7d5701937a188f28615536239efd365b7
Adam Szajgin <01170397@pw.edu.pl>
2023-11-14 11:48:46 +0100

    14/11 commit 1

--- ArithmeticLogicUnit.sv
+++ ArithmeticLogicUnit.sv
@@ -1,25 +1,25 @@
 module ArithmeticLogicUnit (i_op, i_arg_A, i_ar_B);
 
-	parameter N = 8;
-	parameter M = 4;
-
-    	input logic [N:0] i_op;
-    	input logic [M:0] i_arg_A;
-    	input logic [M:0] i_arg_B;
+	parameter N = 2;
+	parameter M = 8;
+	
+    	input logic [N-1:0] i_op;
+    	input logic [M-1:0] i_arg_A;
+    	input logic [M-1:0] i_arg_B;
     	input logic i_clk;
     	input logic i_reset;
-    	output logic [N:0] o_result;
+    	output logic [M-1:0] o_result;
     	output logic [3:0] o_statu;
 
     	logic [N:0] result;
     	logic [3:0] status;
 	
 
-    	always_comb 
+    	always @(posedge i_clk) begin
     	begin
     	
     		case (i_op)
-    			3'b000: // A-2*B
+    			2'b00: // A-2*B
     				result = i_arg - (2 * i_arg_B);
     				status = 4'b0000; //Reset
     				
@@ -27,8 +27,20 @@ module ArithmeticLogicUnit (i_op, i_arg_A, i_ar_B);
     					status[0] = 1;
     				
     				if ((i_arg_A + i_arg_B)[$unsigned(i_arg_B)] == 0)
-                    			status[1] = 1; 
+                    			status[1] = 1;
+                    	2'b01: // A < B
+				
+				
+				                    	
+                    	2'b10: // (A+B)[B] = 0
+                    		
+				                    		
                     			
+                    	2'b11: // U2(A) => ZM(A)
+
+
+			
+			                    			
                     	default:
 		endcase
     	end
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# SCK indiviudual project of ALU
+# SCK indiviudual ALU project
 
-### This is the repository for my project
+arith_unit_4 module working in U2
+
+
+## Operations
+
+Here's a concise description of the operations performed by the Arithmetic Logic Unit (ALU) in SystemVerilog:
+
+
+- A - 2*B
+- A < B
+- (A+B)[B] = 0
+- U2(A) => ZM(A)
+
+## Ports
+
+- i_op: n-bite input deciding of code operatio
+- i_arg_A: m-bit input for argument A
+- i_arg_B: m-bit input for argument B
+- i_clk: Clock input
+- i_reset: Synchronous reset input triggered by a low state
+- o_result: Synchronous output of the system
+- o_status: Synchronous output indicating the status associated with the operation's result
+
+## Status operation bits:
+- ERROR bit: Indicates when the operation result is determined incorrectly.
+- EVEN_1 bit: Indicates when the number of ones in the result is even.
+- ONES bit: Indicates when all bits in the result are set to 1.
+- OVERFLOW bit: Indicates an overflow condition occurred.
